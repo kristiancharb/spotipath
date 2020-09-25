f9e7d7a0e051707a30f721357400cc57b2bc8b0d
Kristian Charbonneau <kristiancharb@gmail.com>
2020-09-24 21:07:46 -0400

    Updates

---
+++ precache-manifest.eea167f13530c401a5b781dbefa5784b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc555d3dda9220fbd39bd9ca27aad46",
+    "url": "/spotipath/index.html"
+  },
+  {
+    "revision": "c059ff0b7a9da00684eb",
+    "url": "/spotipath/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2711cf02375e22d424b1",
+    "url": "/spotipath/static/js/2.196074b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spotipath/static/js/2.196074b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c059ff0b7a9da00684eb",
+    "url": "/spotipath/static/js/main.9bddba79.chunk.js"
+  },
+  {
+    "revision": "e4773a8217fbfa004e26",
+    "url": "/spotipath/static/js/runtime-main.e884f8c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spotipath/static/media/logo.5d5d9eef.svg"
+  }
+]);
