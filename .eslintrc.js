39064292bfba62d2d120c9ee170ffc98ffd229df
Kristian Charbonneau <kristiancharb@gmail.com>
2020-09-26 17:20:59 -0400

    Add lint + editor config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+      "semi": [2, "always"],
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off"
+    }
+};
